e498e1706285cf43d3da2fd74cecc70b9431a57d
김영훈 <hunifive@naver.com>
2023-12-05 00:30:14 +0900

    RESTful CRUD

--- JavaScript/The Web Developer 부트캠프 2023/REST/RESTDemo/index.js
+++ JavaScript/The Web Developer 부트캠프 2023/REST/RESTDemo/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+const methodOverride = require('method-override');
+const { v4: uuid } = require('uuid');
 
 /**
  * <POST>
@@ -15,6 +18,87 @@ app.use(express.urlencoded({ extended: true }));
  * request body 에 JSON 데이터를 제출할 때
  */
 app.use(express.json());
+
+/**
+ * 브라우저의 폼에서는 GET, POST 를 제외하면 보낼 수 없다. (Fetch, Axios 등 으로는 가능)
+ * method-override 를 사용하여 express 에 PUT, DELETE 를 요청할 수 있다.
+ */
+app.use(methodOverride('_method'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+let comments = [
+    {
+        id: uuid(),
+        username: "Todd",
+        comment: 'lol that is so funny!'
+    },
+    {
+        id: uuid(),
+        username: "Skyler",
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        id: uuid(),
+        username: "Sk8erBoi",
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        id: uuid(),
+        username: "onlysayswoof",
+        comment: 'woof woof woof'
+    },
+];
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments });
+});
+
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new');
+})
+
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body;
+    comments.push({ id: uuid(), username, comment });
+    res.redirect('/comments');
+});
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    // id 값이 req.params 에서 오기 때문에 문자열이 된다. (parseInt 사용)
+    // uuid 를 쓰면 문자열이기 때문에 상관 X
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', { comment });
+});
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment });
+});
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText;
+
+    res.redirect('/comments')
+})
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
+});
+
+
+
+
+
+
 
 app.get("/tacos", (req, res) => {
     res.send("GET /tacos response");
